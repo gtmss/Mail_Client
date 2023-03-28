b674439b025896d9ceff0f6ce0e86a796cf6e459
gtm <ion.gatman@esempla.com>
2023-03-28 19:47:59 +0300

    Read mails

---
+++ src/main/java/com/mail/demo/controller/MailController.java
@@ -0,0 +1,2 @@
+package com.mail.demo.controller;public class MailController {
+}
---
+++ src/main/java/com/mail/demo/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.mail.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailReader {
+    
+}
