c7cf5d5d206208c19a1da7b767bfbdd039e26358
gtm <ion.gatman@esempla.com>
2023-03-28 12:52:59 +0300

    first commit

---
+++ src/test/java/com/mail/demo/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
